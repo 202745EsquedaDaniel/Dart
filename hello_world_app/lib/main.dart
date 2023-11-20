4bec423211388acea962268bf94506dfb35edc83
00lar <eduardani84@gmail.com>
2023-11-20 15:26:44 -0700

    AppBar and actions

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
 import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const CounterScreen(),
+      home: const CounterFunctionsScreen(),
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.red),
     );
   }
