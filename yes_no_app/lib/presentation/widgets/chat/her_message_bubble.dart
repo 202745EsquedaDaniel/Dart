34fb46ac9ce0434923372c3976577d56714b80ef
00lar <eduardani84@gmail.com>
2023-11-20 23:26:20 -0700

    loading message

--- yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
@@ -33,6 +33,7 @@ class _ImageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
@@ -40,6 +41,16 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: const Text("Freya esta enviando una imagen"),
+          );
+        },
       ),
     );
   }
